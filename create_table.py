d6ff74441794e2dd339989f85627957a05fbe199
zhu-jz <zhu-jz@users.noreply.github.com>
2023-05-18 19:09:00 +0800

    Add command-line argument support for table name
    The code now accepts a command-line argument in the format `python create_table.py -table=tb_privacy_data` to specify the name of the table to be created. This provides more flexibility in choosing the table name without modifying the source code directly. The `argparse` module is used for argument parsing, and the table creation logic is encapsulated within a function for reusability.

--- create_table.py
+++ create_table.py
@@ -5,34 +5,55 @@ Created on Wed May 10 15:53:30 2023
 @author: ZHU
 """
 
+import argparse
 import psycopg2
 
 
-# Establish the database connection
-conn = psycopg2.connect(
-    host="127.0.0.1",
-    database="postgres",
-    user="postgres",
-    password="password"
-)
-
-table_name = "tb_privacy_data"
-schema = """
-    CREATE TABLE IF NOT EXISTS {table_name} (
-        主订单ID VARCHAR(50) PRIMARY KEY,
-        淘宝ID VARCHAR(50),
-        OpenID VARCHAR(50),
-        BuyerID VARCHAR(50),
-        姓名 VARCHAR(50)[],
-        地址 TEXT[],
-        手机 VARCHAR(50)[]
+def create_table(table_name):
+    # Establish the database connection
+    conn = psycopg2.connect(
+        host="127.0.0.1",
+        database="postgres",
+        user="postgres",
+        password="password"
     )
-""".format(table_name=table_name)
 
-# Execute the schema creation query
-with conn.cursor() as cur:
-    cur.execute(schema)
-    conn.commit()
+    schema = """
+        CREATE TABLE IF NOT EXISTS {table_name} (
+            主订单ID VARCHAR(50) PRIMARY KEY,
+            淘宝ID VARCHAR(50),
+            OpenID VARCHAR(50),
+            BuyerID VARCHAR(50),
+            姓名 VARCHAR(50)[],
+            地址 TEXT[],
+            手机 VARCHAR(50)[]
+        )
+    """.format(table_name=table_name)
 
-# Close the database connection
-conn.close()
+    # Execute the schema creation query
+    with conn.cursor() as cur:
+        cur.execute(schema)
+        conn.commit()
+
+    # Close the database connection
+    conn.close()
+
+
+def main():
+    # Create the argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--table_name", help="Name of the table")
+    args = parser.parse_args()
+
+    # Check if the table name is provided
+    if not args.table_name:
+        parser.error("Please provide the name of the table using the -t/--table_name argument.")
+
+    # Retrieve the table name from the command-line argument
+    table_name = args.table_name
+
+    # Call the create_table function
+    create_table(table_name)
+
+if __name__ == "__main__":
+    main()
